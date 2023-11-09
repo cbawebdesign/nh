3dc459451f3b3150564f72c1d1a81ff780273ce7
cbawebdesign <35353826+cbawebdesign@users.noreply.github.com>
2023-11-08 21:35:25 -0800

    ko

--- src/components/GridList.tsx
+++ src/components/GridList.tsx
@@ -1,4 +1,10 @@
-const GridList: React.FCC = ({ children }) => {
+import React from 'react';
+
+type GridListProps = {
+  children: React.ReactNode; // This type is for anything that React can render
+};
+
+const GridList: React.FC<GridListProps> = ({ children }) => {
   return (
     <div className="mb-16 grid grid-cols-1 gap-y-8 md:grid-cols-2 lg:grid-cols-3 md:gap-x-8 md:gap-y-12 lg:gap-x-12">
       {children}
